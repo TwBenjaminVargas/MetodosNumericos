6d6871e193f62ee15ba12b56eec46a5271433e2f
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2023-12-01 21:26:47 -0300

    Cuadratura de Gauss Añadida

--- Integracion Numerica/Integracion.cpp
+++ Integracion Numerica/Integracion.cpp
@@ -1,7 +1,7 @@
 /**
  * Integracion a traves de diferentes metodos numericos
  * @author Benjamin Vargas
- * @version 2.0
+ * @version 3.0
 */
 #define PRECISION 6 //cantidad de decimales a imprimir
 #include<iostream>
@@ -14,7 +14,7 @@ void trapecio(double**, int);
 void trapecio();
 void simpson();
 void simpson(double**, int);
-
+void cuadraturaGauss();
 double f (double x)
 {
     return pow(x,2)/2;
@@ -77,6 +77,7 @@ int main()
         cout<<"2.Trapecio compuesto nodes\n";
         cout<<"3.Simpson compuesto f(x)\n";
         cout<<"4.Simpson compuesto nodes\n";
+        cout<<"5.Cuadratura Gauss con f(x)\n";
         cout<<"0.Salir\n";
         cin>>op;
         switch (op)
@@ -94,6 +95,9 @@ int main()
             break;
         case 4:
             simpson(nodes,n);
+            break;
+        case 5:
+            cuadraturaGauss();
             break;
         default:
             cout<<"Opcion invalida!\n";
@@ -234,4 +238,91 @@ void simpson(double** nodes, int n) {
 
     }
 
+}
+
+void cuadraturaGauss()
+{
+    int points;
+    double integral, a, b;
+
+
+    cout<<"Cuadratura de Gauss utilizando f(x) definida\n";
+
+    //Factores de ponderacion y argumentos de funcion
+    double two[] = {1, 1, -0.577350269, 0.577350269};
+    
+    double three[] = {0.5555556, 0.8888889, 0.5555556, -0.774596669, 0.0, 0.774596669};
+    
+    double four[] = {0.3478548, 0.6521452, 0.6521452, 0.3478548, -0.861136312, -0.339981044, 0.339981044, 0.861136312};
+    
+    double five[] = {0.2369269, 0.4786287, 0.5688889, 0.4786287, 0.2369269, -0.906179846, -0.538469310, 0.0,
+                     0.538469310, 0.906179846};
+    
+    double six[] = {0.1713245, 0.3607616, 0.4679139, 0.4679139, 0.3607616, 0.1713245, -0.932469514, -0.661209386,
+                    -0.238619186, 0.238619186, 0.661209386, 0.932469514};
+
+
+
+    cout<<"Ingrese el valor del limite de integracion inferior\n";
+    cin>>a;
+    cout<<"Ingrese el valor del limite de integracion superior\n";
+    cin>>b;
+
+    do {
+        cout<<"Ingrese la cantidad de puntos (recuerde que dicho valor esta entre 2 y 6). Ingrese 0 para volver al menu\n";
+        cin>>points;
+        cout<<"\n\n";
+        switch (points) {
+            case 0:
+                break;
+            case 2:
+                integral = ((b - a) / 2) *
+                           (two[0] * f(((b - a) * two[2] + b + a) / 2) + two[1] * f(((b - a) * two[3] + b + a) / 2));
+                cout<<"El valor de la integral es: "<<integral<<"\n";
+                cout<<"Ajusta de manera exacta hasta grado 3";
+                break;
+            case 3:
+                integral = ((b - a) / 2) *
+                           (three[0] * f(((b - a) * three[3] + b + a) / 2) +
+                            three[1] * f(((b - a) * three[4] + b + a) / 2) +
+                            three[2] * f(((b - a) * three[5] + b + a) / 2));
+                
+                cout<<"El valor de la integral es: "<<integral<<"\n";
+                cout<<"Ajusta de manera exacta hasta grado 5";
+                break;
+            case 4:
+                integral = ((b - a) / 2) *
+                           (four[0] * f(((b - a) * four[4] + b + a) / 2) +
+                            four[1] * f(((b - a) * four[5] + b + a) / 2) +
+                            four[2] * f(((b - a) * four[6] + b + a) / 2) +
+                            four[3] * f(((b - a) * four[7] + b + a) / 2));
+                cout<<"El valor de la integral es: "<<integral<<"\n";
+                cout<<"Ajusta de manera exacta hasta grado 7";
+                break;
+            case 5:
+                integral = ((b - a) / 2) *
+                           (five[0] * f(((b - a) * five[5] + b + a) / 2) +
+                            five[1] * f(((b - a) * five[6] + b + a) / 2) +
+                            five[2] * f(((b - a) * five[7] + b + a) / 2) +
+                            five[3] * f(((b - a) * five[8] + b + a) / 2) +
+                            five[4] * f(((b - a) * five[9] + b + a) / 2));
+                cout<<"El valor de la integral es: "<<integral<<"\n";
+                cout<<"Ajusta de manera exacta hasta grado 9";
+                break;
+            case 6:
+                integral = ((b - a) / 2) *
+                           (six[0] * f(((b - a) * six[6] + b + a) / 2) +
+                            six[1] * f(((b - a) * six[7] + b + a) / 2) +
+                            six[2] * f(((b - a) * six[8] + b + a) / 2) +
+                            six[3] * f(((b - a) * six[9] + b + a) / 2) +
+                            six[4] * f(((b - a) * six[10] + b + a) / 2) +
+                            six[5] * f(((b - a) * six[11] + b + a) / 2));
+                cout<<"El valor de la integral es: "<<integral<<"\n";
+                cout<<"Ajusta de manera exacta hasta grado 11";
+                break;
+            default:
+                printf("La opcion ingresada no es valida. Porfavor ingrese una opcion valida\n");
+                break;
+        }
+    } while (points != 0);
 }
